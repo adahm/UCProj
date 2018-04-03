a73a2e6dc2f65f5afc0c936494160e7a27dbc63f
Adam Hasselberg <adamhasselberg@gmail.com>
2018-04-03 11:34:25 +0200

    first runable version

--- app/src/main/java/com/example/andreas/iridiumflares/MainActivity.java
+++ app/src/main/java/com/example/andreas/iridiumflares/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.andreas.iridiumflares;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.ListActivity;
+import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+//import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] myStringArray = {"test"};
+        ListView FlareList = findViewById(R.id.list);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, myStringArray);
+
+        FlareList.setAdapter(adapter);
+
     }
+
 }
