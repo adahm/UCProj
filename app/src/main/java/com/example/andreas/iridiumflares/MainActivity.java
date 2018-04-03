e2438f8ed56bb3e42ff00a5be7db3dd5f6569612
Adam Hasselberg <adamhasselberg@gmail.com>
2018-04-03 11:53:05 +0200

    day 1 done

--- app/src/main/java/com/example/andreas/iridiumflares/MainActivity.java
+++ app/src/main/java/com/example/andreas/iridiumflares/MainActivity.java
@@ -8,6 +8,8 @@ import android.content.Loader;
 import android.database.Cursor;
 //import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
@@ -27,6 +29,27 @@ public class MainActivity extends Activity {
                 android.R.layout.simple_list_item_1, myStringArray);
 
         FlareList.setAdapter(adapter);
+
+        FlareList.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> adapter, View v, int position,
+                                    long arg3)
+            {
+                String value = (String)adapter.getItemAtPosition(position);
+                // assuming string and if you want to get the value on click of list item
+                // do what you intend to do on click of listview row
+                System.out.println("menu item selected: " + value);
+
+                /*
+                Intent intent = new Intent(this, VÅRAN-EGEN-KLASS.class);
+                ****KOORDINATER****
+                intent.putExtra(EXTRA_MESSAGE, message);
+                startActivity(intent);
+                 */
+
+            }
+        });
 
     }
 
