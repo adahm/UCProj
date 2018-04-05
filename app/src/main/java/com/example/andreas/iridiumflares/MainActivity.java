38021bcb1fbdee8ef43ebe726d3b229dc0486bc5
Adam Hasselberg <adamhasselberg@gmail.com>
2018-04-05 09:42:16 +0200

    gps location

--- app/src/main/java/com/example/andreas/iridiumflares/MainActivity.java
+++ app/src/main/java/com/example/andreas/iridiumflares/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.andreas.iridiumflares;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 //import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -12,11 +18,35 @@ import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
+    // Declaring a Location Manager
+    protected LocationManager mLocationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                1);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.i("i", "NO GPS PERMISSION");
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location currentLocation = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        double currentLatitude = currentLocation.getLatitude();
+        double currentLongitude = currentLocation.getLongitude();
+        Log.i("i", "Longitude: " + currentLongitude + " Latitude: " + currentLatitude);
+
 
         String[] myStringArray = {"test"};
         ListView FlareList = findViewById(R.id.list);
