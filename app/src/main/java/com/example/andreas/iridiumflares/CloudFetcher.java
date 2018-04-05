da42f5f9d23017a4eee7199f4086b4196e3d1cb5
adahm <18390656+adahm@users.noreply.github.com>
2018-04-05 11:41:12 +0200

    fetch SMhi data

---
+++ app/src/main/java/com/example/andreas/iridiumflares/CloudFetcher.java
@@ -0,0 +1,58 @@
+package com.example.andreas.iridiumflares;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class CloudFetcher {
+    double longitude;
+    double latitude;
+
+    public CloudFetcher(double longitude, double latitude) {
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+
+    public ArrayList<Flares> cloudCheck(ArrayList<Flares> flareList){
+        private String metObsAPI = "https://opendata-download-metobs.smhi.se/api";
+        JSONObject parameterObject = readJsonFromUrl(metObsAPI + "/version/latest/geotype/point/lon/{"+ longitude+"/lat/"+latitude+"/data.json";
+
+        for (Flares f: flareList) {
+
+        }
+    }
+
+    public Boolean checkFlare(Flares f){
+        //TODO gör en jsonarry som går över timeseries
+        //
+
+    }
+    private JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+        String text = readStringFromUrl(url);
+        return new JSONObject(text);
+    }
+    private String readStringFromUrl(String url) throws IOException {
+
+        InputStream inputStream = new URL(url).openStream();
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, Charset.forName("UTF-8")));
+            StringBuilder stringBuilder = new StringBuilder();
+            int cp;
+            while ((cp = reader.read()) != -1) {
+                stringBuilder.append((char) cp);
+            }
+            return stringBuilder.toString();
+        } finally {
+            inputStream.close();
+        }
+    }
+
+}
