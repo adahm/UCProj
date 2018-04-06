dc2980473995369c23d6510ab5b9eb30066c30ff
adahm <18390656+adahm@users.noreply.github.com>
2018-04-06 11:03:49 +0200

    fixed parsing of data from SMHI and now flares that will be covered are removed

--- app/src/main/java/com/example/andreas/iridiumflares/CloudFetcher.java
+++ app/src/main/java/com/example/andreas/iridiumflares/CloudFetcher.java
@@ -1,5 +1,9 @@
 package com.example.andreas.iridiumflares;
 
+import org.joda.time.DateTime;
+import org.joda.time.LocalDateTime;
+import org.joda.time.Period;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -9,7 +13,10 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.nio.charset.Charset;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+
 import java.util.Date;
 
 public class CloudFetcher {
@@ -21,12 +28,60 @@ public class CloudFetcher {
         this.latitude = latitude;
     }
 
-    public void cloudCheck(ArrayList<Flares> flareList) throws IOException, JSONException {
-        String metObsAPI = "https://opendata-download-metobs.smhi.se/api";
-        JSONObject parameterObject = readJsonFromUrl(metObsAPI + "/version/latest/geotype/point/lon/{"+ longitude+"/lat/"+latitude+"/data.json");
+    public void cloudCheck(ArrayList<Flares> flareList) throws IOException, JSONException, ParseException {
+        String URL = "https://opendata-download-metfcst.smhi.se/api";
+        JSONObject parameterObject = readJsonFromUrl(URL + "/category/pmp3g/version/2/geotype/point/lon/"+ longitude+"/lat/"+latitude+"/data.json");
+        JSONArray wheterArray = parameterObject.getJSONArray("timeSeries");
+        JSONObject firstTime;
+        JSONObject endTime;
+        SimpleDateFormat df = new SimpleDateFormat( "yyyy-MM-dd'T'HH:mm:ssz" );
+        //TODO create parser to get the time from the jsobobject
 
+        //loop though every flare
         for (Flares f: flareList) {
+            //for every flare find the best time by looping though the times in the timesseries list
+            //when we reach a timestamp where the first time is before and the next time is after stop
+            //compare the time difference between the time before and the time after
+            //Select time with the smallest difference
+            //get the tcc_mean value for that time
+            //if it is greater than 4 remove the flare from the array
+            //otherwise keep it and contiune with the next flare
+            Date flareDate =f.getDate();
 
+            for (int i = 0;wheterArray.length()-1>i;i++){
+                firstTime = wheterArray.getJSONObject(i);
+                endTime = wheterArray.getJSONObject(i+1);
+                LocalDateTime l1 = new LocalDateTime(firstTime.getString("validTime").split("Z")[0]);
+                LocalDateTime l2 = new LocalDateTime(endTime.getString("validTime").split("Z")[0]);
+                LocalDateTime l3 = new LocalDateTime(flareDate);
+
+
+
+                String time1 = firstTime.getString("validTime");
+                String time2 = endTime.getString("validTime");
+                if(l1.isBefore(l3) && l2.isAfter(l3)){
+
+                    Period p1 = new Period(l1, l3);
+                    Period p2 = new Period(l3, l2);
+
+                    int hours1 = p1.getHours();
+                    int hours2 = p2.getHours();
+
+                    int cloudcover;
+                    if(hours1<hours2){
+                        cloudcover = firstTime.getJSONArray("parameters").getJSONObject(7).getJSONArray("values").getInt(0);
+
+                    }
+                    else {
+                        cloudcover = endTime.getJSONArray("parameters").getJSONObject(7).getJSONArray("values").getInt(0);
+
+                    }
+                    if(cloudcover>4){
+                        flareList.remove(f);
+                    }
+                    break;
+                }
+            }
         }
     }
 
@@ -53,6 +108,20 @@ public class CloudFetcher {
             return stringBuilder.toString();
         } finally {
             inputStream.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        //TODO ta emot listan som skickas istället
+        CloudFetcher c = new CloudFetcher(18,59);
+        try {
+            c.cloudCheck(new ArrayList<Flares>());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
         }
     }
 
